170e6b50119d89d9d7f33174fc0fc8ae1f708a73
DESKTOP-FHRENU6\Alexander <Alexan2405@gmail.com>
2019-11-06 21:37:30 +0100

    no message

---
+++ Assets/Mountain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mountain : MonoBehaviour
+{
+    public Animator LeftEye;
+    public Animator RightEye;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayEyeAnimation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator PlayEyeAnimation()
+    {
+        yield return new WaitForSeconds(5);
+        LeftEye.SetBool("Run", true);
+        RightEye.SetBool("Run", true);
+        StartCoroutine(PlayEyeAnimation());
+    }
+}
